075f95d632e857503aa4c925a01df70493d8024e
Alvin Galit <aroblesgalit@gmail.com>
2018-05-30 04:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArr = [];
   
-  for (var i = 0; i < musicians.length; i++)
+  for (var i = 0; i < musicians.length; i++) {
+    newArr
+  }
 }
